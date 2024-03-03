4e946cb31386e0fc9b27906cf317d9cee4a03597
Thorsten Staerk <dev@staerk.de>
2024-03-03 18:50:55 +0100

    keyboard input in bootsector

---
+++ input.s
@@ -0,0 +1,22 @@
+start:
+; this should print H
+    mov ax, 0xe48 ; al=48=H ax=e=output a string
+    int 0x10
+; E
+    mov al, 0x45
+    int 0x10
+; L
+    mov al, 0x4C
+    int 0x10
+; L
+    mov al, 0x4C
+    int 0x10
+; O
+    mov al, 0x4F
+    int 0x10
+    mov ah, 0
+    int 0x16
+    mov ah,0xe
+    int 0x10
+.ende:
+    jmp .ende
